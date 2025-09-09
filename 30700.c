eca36256f83a5d57de6bd1a45ac71e50346f634f
sh2624 <sh2624@daum.net>
2025-09-09 11:52:59 +0900

    [250909] 'KOREA 문자열 만들기'

---
+++ 30700.c
@@ -0,0 +1,28 @@
+// [브론즈 2] 30700 - KOREA 문자열 만들기
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    // 문자열 입력
+    char s[1001];
+    scanf("%s", s);
+
+    char korea[6] = "KOREA";
+    int cnt = 0, len = strlen(s);
+
+    // 문자열 돌면서 해당 글자가 있는지 검사
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (korea[cnt % 5] == s[i])
+            cnt++;
+    }
+
+    // 결과 출력
+    printf("%d", cnt);
+
+    system("pause");
+    return 0;
+}
